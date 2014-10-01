a69117106c7d3afbd807c1cda5f4d6c5a11b826a
Emily Chen <tenshiemi@gmail.com>
2014-10-01 11:04:31 +0200

    fixed type cohersion test

--- exercises/language/1.numbers_spec.js
+++ exercises/language/1.numbers_spec.js
@@ -39,7 +39,7 @@ describe('Working with Numbers', function() {
     var string   = '5';
     var equality = number == string;
 
-    assert.equal(equality, false);
+    assert.equal(equality, true);
   })
 
   it('equality with type cohersion -null-', function() {
